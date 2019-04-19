11ae834aa5078c31e1eaf8a96ebfdc0a98174131
Vladimir Ovechkin <ovchingus@gmail.com>
2019-04-19 17:52:39 +0300

    Initial commit

--- src/app.js
+++ src/app.js
@@ -6,13 +6,25 @@ const budgetController = (function () {
 
 // UI controller
 const UIController = (function () {
+
+    const DOMStrings = {
+        inputType: ".add__type",
+        inputDescription: ".add__description",
+        inputValue: ".add__value",
+        inputBtn: ".add__btn"
+    };
+
     return {
-        getinput: function () {
+        getInput: function () {
             return {
-                type: document.querySelector(".add__type").value, //Will be inc or exp
-                description: document.querySelector(".add__description").value,
-                value: document.querySelector(".add__value").value
+                type: document.querySelector(DOMStrings.inputType).value, //Will be inc or exp
+                description: document.querySelector(DOMStrings.inputDescription).value,
+                value: document.querySelector(DOMStrings.inputValue).value
             }
+        },
+
+        getDOMStrings: function () {
+            return DOMStrings;
         }
     }
 })();
@@ -21,8 +33,12 @@ const UIController = (function () {
 // Global app controller
 const controller = (function (budgetCtrl, UICtrl) {
 
+    const DOM = UICtrl.getDOMStrings();
+
     const ctrlAddItem = function () {
         // 1. get the filled input data
+        const input = UICtrl.getInput();
+        console.log(input);
 
         // 2. Add the item to the budget controller
 
@@ -30,10 +46,10 @@ const controller = (function (budgetCtrl, UICtrl) {
 
         // 4. Calculate the budget
 
-        // 5. Displey the budget on the UI
+        // 5. Display the budget on the UI
     };
 
-    document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
 
     document.addEventListener("keypress", function (event) {
         if (event.keyCode === 13 || event.which === 13) {
