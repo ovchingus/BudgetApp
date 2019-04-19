4bf8519ae87e61982f5bb5f78368e6093e722ba7
Vladimir Ovechkin <ovchingus@gmail.com>
2019-04-19 18:30:09 +0300

    Add addItem method to budgetController

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 // Budget controller
 const budgetController = (function () {
 
-    const Expense = function(id, description, value) {
-        this.id= id;
+    const Expense = function (id, description, value) {
+        this.id = id;
         this.description = description;
         this.value = value;
     };
 
-    const Income = function(id, description, value) {
-        this.id= id;
+    const Income = function (id, description, value) {
+        this.id = id;
         this.description = description;
         this.value = value;
     };
@@ -21,6 +21,39 @@ const budgetController = (function () {
         total: {
             exp: 0,
             inc: 0
+        }
+    };
+
+    return {
+        addItem: function (type, des, val) {
+            let newItem, ID;
+
+            // [1 2 3 4 5], next ID = 6
+            // [1 2 4 6 8], next ID = 9
+            // ID = lastID + 1
+
+            // Create new ID
+            if (data.flow[type].length === 0) {
+                ID = 0;
+            } else {
+                ID = data.flow[type][data.flow[type].length - 1].id + 1;
+            }
+            // Create new item based on 'inc' or 'exp' type
+            if (type === "exp") {
+                newItem = new Expense(ID, des, val)
+            } else if (type === "inc") {
+                newItem = new Income(ID, des, val)
+
+            }
+
+            // Push it into our data structure
+            data.flow[type].push(newItem);
+
+            // Return the new element
+            return newItem;
+        }, testing: function () {
+
+            console.log(data);
         }
     }
 })();
@@ -69,10 +102,12 @@ const controller = (function (budgetCtrl, UICtrl) {
     };
 
     const ctrlAddItem = function () {
+
         // 1. get the filled input data
         const input = UICtrl.getInput();
 
         // 2. Add the item to the budget controller
+        const newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // 3. Add the item to the UI
 
