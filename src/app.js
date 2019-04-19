7b3a36d52003aa2290b230a4a697088272c9f758
Vladimir Ovechkin <ovchingus@gmail.com>
2019-04-19 18:09:12 +0300

    Add budgetController constructor and data structures

--- src/app.js
+++ src/app.js
@@ -1,6 +1,28 @@
 // Budget controller
 const budgetController = (function () {
 
+    const Expense = function(id, description, value) {
+        this.id= id;
+        this.description = description;
+        this.value = value;
+    };
+
+    const Income = function(id, description, value) {
+        this.id= id;
+        this.description = description;
+        this.value = value;
+    };
+
+    const data = {
+        flow: {
+            exp: [],
+            inc: []
+        },
+        total: {
+            exp: 0,
+            inc: 0
+        }
+    }
 })();
 
 
